1d0899afdad03938f2bf1b936e34d2678e8e28e3
NaFFF345 <asjd581@gmail.com>
2018-02-06 11:24:40 +0900

    [add]inital commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FroBook</title>
+    <link rel="stylesheet" src="style.css">
+</head>
+<body>
+    <header>
+    <h1>FroBook</h1>
+    
+    </header>
+</body>
+</html>
